e0f9f669e27b5c9d72eb7b1c3cf82507e289e4a3
Basith-08 <asfinebasith@gmail.com>
2024-12-04 16:11:14 +0700

    udapate pertemuan 7

---
+++ pertemuan-7/index.php
@@ -0,0 +1,30 @@
+<?php
+include('koneksi.php');
+?>
+
+<div class="container-md py-4">
+  <table class="table">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Nim</th>
+        <th scope="col">Nama</th>
+        <th scope="col">Prodi</th>
+        <th scope="col">Jenis Kelamin</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th scope="row">1</th>
+        <td>1003230001</td>
+        <td>Yoga</td>
+        <td>Informatika</td>
+        <td>Laki - laki</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+
+<?php
+include('footer.php');
+?>
